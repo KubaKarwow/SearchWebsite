8e4d511b96ff633ba19825d3670439b1607342b4
Jakub <kubakarwow@gmail.com>
2024-05-29 15:18:50 +0200

    making 3 tables in DB instead of one

--- demo/src/main/java/pjatk/tpo/demo/GamerRepository.java
+++ demo/src/main/java/pjatk/tpo/demo/GamerRepository.java
@@ -1,25 +1,31 @@
 package pjatk.tpo.demo;
 
 import java.io.*;
+import java.nio.file.Paths;
 import java.sql.*;
 
-public class GamerRepository
-{
+public class GamerRepository {
     private Connection connection;
+
     public GamerRepository(Connection connection) {
-        this.connection=connection;
+        this.connection = connection;
     }
 
-    public void initializeDB() throws SQLException {
+    public void initializeDB() throws SQLException, IOException {
         Statement statement = connection.createStatement();
-        statement.executeUpdate("Drop table Gamer");
-        statement.executeUpdate("Create Table Gamer(ID INT PRIMARY KEY," +
-                " NAME VARCHAR(20),SURNAME VARCHAR(30),NICK VARCHAR(20), AGE INT" +
-                ",CURRENTTEAM VARCHAR(30),REGION VARCHAR(20),REGIONALWINS INT, INTERNATIONALWINS INT, PHOTO BLOB)");
-
+        if(!doesTableExist(connection,"Team")){
+            statement.executeUpdate("CREATE TABLE TEAM(ID INT PRIMARY KEY, NAME VARCHAR(20),CREATED DATE, ,)")
+        }
+        if (!doesTableExist(connection, "Gamer")) {
+            statement.executeUpdate("Create Table Gamer(ID INT PRIMARY KEY," +
+                    " NAME VARCHAR(20),SURNAME VARCHAR(30),NICK VARCHAR(20), AGE INT" +
+                    ",CURRENTTEAM VARCHAR(30),REGION VARCHAR(20),REGIONALWINS INT, INTERNATIONALWINS INT, PHOTO BLOB)");
+            addRecords();
+        }
     }
-    public void addRecord(int id, String name, String surname, String nick,int age, String currentTeam, String region,
-                          int regionalTrophies, int internationalTrophies, File imageFile) throws SQLException, IOException {
+
+    private void addRecord(int id, String name, String surname, String nick, int age, String currentTeam, String region,
+                           int regionalTrophies, int internationalTrophies, File imageFile) throws SQLException, IOException {
         Connection connection = DriverManager.getConnection("jdbc:derby:Gamers;create=true");
         String sql = "INSERT INTO GAMER VALUES(?,?,?,?,?,?,?,?,?,?)";
         PreparedStatement statement = connection.prepareStatement(sql);
@@ -34,7 +40,124 @@ public class GamerRepository
         statement.setInt(9, internationalTrophies);
         InputStream inputStream = new FileInputStream(imageFile);
         statement.setBlob(10, inputStream);
-
         statement.executeUpdate();
+    }
+
+    private boolean doesTableExist(Connection connection, String tableName) throws SQLException {
+        ResultSet tables = connection.getMetaData().getTables(null, null, tableName, null);
+        return tables.getFetchSize() == 1;
+    }
+
+    private void addRecords() throws SQLException, IOException {
+        addRecord(1, "Lee", "Sang-hyeok", "Faker", 28, "T1", "South Korea", 10, 6,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(2, "Choi ", "Woo-je", "Zeus", 20, "T1", "South Korea", 4, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(3, "Mun ", "Hyeon-jun", "Oner", 21, "T1", "South Korea", 4, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(4, "Lee", "Min-hyeong", "Gumayusi", 22, "T1", "South Korea", 4, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(5, "Ryu", "Min-seok", "Keria", 21, "T1", "South Korea", 4, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(6, "Sergen ", "Çelik", "BrokenBlade", 24, "G2", "Europe", 6, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(7, "Martin", "Sundelin", "Yike", 23, "G2", "Europe", 5, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(8, "Rasmus", "Winther", "Caps", 24, "G2", "Europe", 12, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(9, "Steven", "Liv", "Hans sama", 24, "G2", "Europe", 5, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(10, "Mihael", "Mehle", "Mikyx", 25, "G2", "Europe", 9, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(11, "Kim", "Gi-in", "Kiin", 25, "GENG", "South Korea", 1, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(12, "Kim", "Geon-bu", "Canyon", 23, "GENG", "South Korea", 3, 2,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(13, "Jeong", "Ji-hoon", "Chovy", 23, "GENG", "South Korea", 4, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(14, "Kim", "Su-hwan", "Peyz", 18, "GENG", "South Korea", 4, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(15, "Son", "Si-woo", "Peyz", 25, "GENG", "South Korea", 2, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(16, "Óscar", "Jiménez", "Oscarinin", 20, "Fnatic", "Europe", 0, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(17, "Iván", "Díaz", "Razork", 23, "Fnatic", "Europe", 0, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(18, "Marek", "Brázda", "Humanoid", 24, "Fnatic", "Europe", 2, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(19, "Oh", "Hyeon-taek", "Noah", 22, "Fnatic", "Europe", 0, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(20, "Yoon", "Se-junk", "Jun", 23, "Fnatic", "Europe", 0, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(21, "Martin", "Hansen", "Wunder", 25, "Heretics", "Europe", 4, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(22, "Marcin", "Jankowski", "Jankos", 28, "Heretics", "Europe", 5, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(23, "Luka", "Perković", "Perkz", 25, "Heretics", "Europe", 9, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(24, "Victor", "Tortosa", "Flakked", 23, "Heretics", "Europe", 1, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(25, "Adrian", "Trybus", "Trymbi", 23, "Heretics", "Europe", 1, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(26, "Alex", "Villarejo", "Myrwn", 20, "MAD Lions KOI", "Europe", 0, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(27, "Javier", "Batalla", "Elyoya", 24, "MAD Lions KOI", "Europe", 3, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(28, "Bartłomiej", "Przewoźnik", "Fresskowy", 24, "MAD Lions KOI", "Europe", 0, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(29, "David", "García", "Supa", 23, "MAD Lions KOI", "Europe", 0, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(30, "Álvaro", "del Amo", "Alvaro", 20, "MAD Lions KOI", "Europe", 0, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(31, "Chen", "Ze-Bin", "Bin", 20, "BLG", "China", 2, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(32, "Peng", "Li-Xun", "Xun", 22, "BLG", "China", 1, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(33, "Zhuo", "Ding", "Knight", 24, "BLG", "China", 4, 1,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(34, "Zhao", "Jia-Hao", "Elk", 22, "BLG", "China", 1, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(35, "Luo", "Wen-Jun", "ON", 21, "BLG", "China", 1, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(36, "Jakub", "Karwowski", "Krol Ringu", 20, "100kg bench press", "Europe", 0, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
+
+        addRecord(37, "Martin", "Larsson", "Rekkles", 27, "T1 Academy", "South Korea", 4, 0,
+                new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
     }
 }
--- demo/src/main/java/pjatk/tpo/demo/HelloServlet.java
+++ demo/src/main/java/pjatk/tpo/demo/HelloServlet.java
@@ -14,9 +14,10 @@ import jakarta.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
     private GamerRepository gamerRepository;
     private Connection connection;
+
     public void init() {
         try {
-            connection= DriverManager.getConnection("jdbc:derby:Gamers;create=true");
+            connection = DriverManager.getConnection("jdbc:derby:Gamers;create=true");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -24,116 +25,7 @@ public class HelloServlet extends HttpServlet {
         try {
             gamerRepository.initializeDB();
 
-            gamerRepository.addRecord(1,"Lee","Sang-hyeok","Faker",28,"T1","South Korea",10,6,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
 
-            gamerRepository.addRecord(2,"Choi ","Woo-je","Zeus",20,"T1","South Korea",4,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(3,"Mun ","Hyeon-jun","Oner",21,"T1","South Korea",4,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(4,"Lee","Min-hyeong","Gumayusi",22,"T1","South Korea",4,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(5,"Ryu","Min-seok","Keria",21,"T1","South Korea",4,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(6,"Sergen ","Çelik","BrokenBlade",24,"G2","Europe",6,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(7,"Martin","Sundelin","Yike",23,"G2","Europe",5,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(8,"Rasmus","Winther","Caps",24,"G2","Europe",12,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(9,"Steven","Liv","Hans sama",24,"G2","Europe",5,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(10,"Mihael","Mehle","Mikyx",25,"G2","Europe",9,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(11,"Kim","Gi-in","Kiin",25,"GENG","South Korea",1,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(12,"Kim","Geon-bu","Canyon",23,"GENG","South Korea",3,2,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(13,"Jeong","Ji-hoon","Chovy",23,"GENG","South Korea",4,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(14,"Kim","Su-hwan","Peyz",18,"GENG","South Korea",4,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(15,"Son","Si-woo","Peyz",25,"GENG","South Korea",2,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(16,"Óscar","Jiménez","Oscarinin",20,"Fnatic","Europe",0,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(17,"Iván","Díaz","Razork",23,"Fnatic","Europe",0,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(18,"Marek","Brázda","Humanoid",24,"Fnatic","Europe",2,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(19,"Oh","Hyeon-taek","Noah",22,"Fnatic","Europe",0,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(20,"Yoon","Se-junk","Jun",23,"Fnatic","Europe",0,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(21,"Martin","Hansen","Wunder",25,"Heretics","Europe",4,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(22,"Marcin","Jankowski","Jankos",28,"Heretics","Europe",5,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(23,"Luka","Perković","Perkz",25,"Heretics","Europe",9,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(24,"Victor","Tortosa","Flakked",23,"Heretics","Europe",1,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(25,"Adrian","Trybus","Trymbi",23,"Heretics","Europe",1,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(26,"Alex","Villarejo","Myrwn",20,"MAD Lions KOI","Europe",0,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(27,"Javier","Batalla","Elyoya",24,"MAD Lions KOI","Europe",3,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(28,"Bartłomiej","Przewoźnik","Fresskowy",24,"MAD Lions KOI","Europe",0,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(29,"David","García","Supa",23,"MAD Lions KOI","Europe",0,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(30,"Álvaro","del Amo","Alvaro",20,"MAD Lions KOI","Europe",0,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(31,"Chen","Ze-Bin","Bin",20,"BLG","China",2,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(32,"Peng","Li-Xun","Xun",22,"BLG","China",1,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(33,"Zhuo","Ding","Knight",24,"BLG","China",4,1,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(34,"Zhao","Jia-Hao","Elk",22,"BLG","China",1,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(35,"Luo","Wen-Jun","ON",21,"BLG","China",1,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(36,"Jakub","Karwowski","Krol Ringu",20,"100kg bench press","Europe",0,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
-
-            gamerRepository.addRecord(37,"Martin","Larsson","Rekkles",27,"T1 Academy","South Korea",4,0,
-                    new File(String.valueOf(Paths.get("C:\\Users\\Jakub\\Desktop\\Studia\\4 semestr\\TPO\\TPO6\\demo\\src\\main\\resources\\PlayerImages\\Faker.png"))));
         } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
@@ -153,10 +45,10 @@ public class HelloServlet extends HttpServlet {
         String region = parameterMap.get("region")[0];
         String sortBy = parameterMap.get("sort")[0];
         for (String s : parameterMap.keySet()) {
-            System.out.println("key:"+s);
+            System.out.println("key:" + s);
             System.out.println("values");
             for (String string : parameterMap.get(s)) {
-                System.out.println("value:"+string);
+                System.out.println("value:" + string);
             }
         }
         PrintWriter out = response.getWriter();
@@ -168,38 +60,39 @@ public class HelloServlet extends HttpServlet {
         System.out.println(international_trophies_min);
         System.out.println(international_trophies_max);
         System.out.println(region);
-        System.out.println("SELECT * FROM GAMER WHERE AGE>="+ageMin+" AND AGE<="+ageMax+" AND " +
-                "REGIONALWINS>="+regional_trophies_min+" AND REGIONALWINS<="+regional_trophies_max+" AND INTERNATIONALWINS>=" +international_trophies_min+
-                " AND INTERNATIONALWINS<="+international_trophies_max+" AND REGION="+region+" ORDER BY "+sortBy);
+        System.out.println("SELECT * FROM GAMER WHERE AGE>=" + ageMin + " AND AGE<=" + ageMax + " AND " +
+                "REGIONALWINS>=" + regional_trophies_min + " AND REGIONALWINS<=" + regional_trophies_max + " AND INTERNATIONALWINS>=" + international_trophies_min +
+                " AND INTERNATIONALWINS<=" + international_trophies_max + " AND REGION=" + region + " ORDER BY " + sortBy);
         try {
             String sql = "SELECT * FROM GAMER WHERE AGE>=? AND AGE<=? AND " +
                     "REGIONALWINS>=? AND REGIONALWINS<=? AND INTERNATIONALWINS>=? " +
-                    " AND INTERNATIONALWINS<=? AND REGION=? "+" ORDER BY "+sortBy;
+                    " AND INTERNATIONALWINS<=? AND REGION=? " + " ORDER BY " + sortBy;
             PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1,ageMin);
-            statement.setInt(2,ageMax);
-            statement.setInt(3,regional_trophies_min);
-            statement.setInt(4,regional_trophies_max);
-            statement.setInt(5,international_trophies_min);
-            statement.setInt(6,international_trophies_max);
-            statement.setString(7,region);
+            statement.setInt(1, ageMin);
+            statement.setInt(2, ageMax);
+            statement.setInt(3, regional_trophies_min);
+            statement.setInt(4, regional_trophies_max);
+            statement.setInt(5, international_trophies_min);
+            statement.setInt(6, international_trophies_max);
+            statement.setString(7, region);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                sendFormattedResponse(out,resultSet);
+                sendFormattedResponse(out, resultSet);
             }
             out.println("<html></body>");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
     private void sendFormattedResponse(PrintWriter out, ResultSet resultSet) throws SQLException, IOException {
         StringBuilder sbd = new StringBuilder();
         for (int i = 1; i < resultSet.getMetaData().getColumnCount(); i++) {
-            out.println("<h3>" +getProperResponse(i)+":"+ resultSet.getString(i) + "</h3>");
+            out.println("<h3>" + getProperResponse(i) + ":" + resultSet.getString(i) + "</h3>");
         }
         sbd = new StringBuilder();
         Blob photo = resultSet.getBlob("PHOTO");
-        if(photo!=null){
+        if (photo != null) {
             byte[] photoBytes = blobToBytes(photo);
             String base64Image = Base64.getEncoder().encodeToString(photoBytes);
             sbd.append("<img src='data:image/jpeg;base64,").append(base64Image).append("' />");
@@ -207,8 +100,9 @@ public class HelloServlet extends HttpServlet {
         out.println("<h3>" + sbd.toString() + "</h3>");
 
     }
-    private String getProperResponse(int columnIndex){
-        switch (columnIndex){
+
+    private String getProperResponse(int columnIndex) {
+        switch (columnIndex) {
             case 1 -> {
                 return "ID";
             }
@@ -241,9 +135,10 @@ public class HelloServlet extends HttpServlet {
             }
         }
     }
+
     private byte[] blobToBytes(Blob blob) throws SQLException, IOException {
         try (InputStream is = blob.getBinaryStream();
-             ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+             ByteArrayOutputStream os = new ByteArrayOutputStream()){
             byte[] buffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = is.read(buffer)) != -1) {
@@ -252,5 +147,7 @@ public class HelloServlet extends HttpServlet {
             return os.toByteArray();
         }
     }
-    public void destroy() {}
+
+    public void destroy() {
+    }
 }
